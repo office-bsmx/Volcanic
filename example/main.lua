09f819aa289b38fb1ae13f82105bc78fa44ec06a
office-bsmx <62848374+office-bsmx@users.noreply.github.com>
2023-10-28 08:14:33 +0300

    Update main.lua

--- example/main.lua
+++ example/main.lua
@@ -1,6 +1,6 @@
 
-function Main(route)
+function Main(route) --default entry point
     local a = string.len(route)
     a = a*12/(100-20)^4.2
-    return tostring("The result is "..a)
+    return tostring("The result is "..a) --returned value must be always a string
 end
